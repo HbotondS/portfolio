91753b19e9b350ebe72f2a0b6567d2812436fa81
Botond Hegyi <botond.hegyi@msg.group>
2020-09-29 20:51:02 +0300

    Updates

---
+++ precache-manifest.16a9669224ecf36771245b390799df25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a8ec730c55f2399aa4589110b79809",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "39528876b491e8484f88",
+    "url": "/portfolio/static/css/main.8c3a8c98.chunk.css"
+  },
+  {
+    "revision": "b8d82f28ccff5d276b1d",
+    "url": "/portfolio/static/js/2.5d634bed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.5d634bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39528876b491e8484f88",
+    "url": "/portfolio/static/js/main.c8e0778b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bd196535814a9adc08a6c056244a3938.js"
+  "/portfolio/precache-manifest.16a9669224ecf36771245b390799df25.js"
 );
 
 self.addEventListener('message', (event) => {
