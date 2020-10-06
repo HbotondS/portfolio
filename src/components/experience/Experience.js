efaa8a90075071ad16dfe7229539f073f25429b9
HbotondS <hbotond97@gmail.com>
2020-10-06 17:42:25 +0300

    add svg illustrations to experience page

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,10 +1,15 @@
 import './Experience.scss'
 import * as React from "react";
+import { ReactComponent as ReadingSvg} from '../../assets/reading.svg';
+import { ReactComponent as WorkSvg} from '../../assets/work.svg';
 
 export class Experience extends React.Component {
     render() {
         return (
-            <div id={'experience'} className={'ExperiencePage'}></div>
+            <div id={'experience'} className={'ExperiencePage'}>
+              <ReadingSvg className={'reading-svg'}/>
+              <WorkSvg className={'work-svg'}/>
+            </div>
         );
     }
 }
