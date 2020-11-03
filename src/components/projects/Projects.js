1aa940ba72210b1b5c78dbe03d0f0692564881c4
HbotondS <hbotond97@gmail.com>
2020-11-03 15:00:58 +0200

    add images for projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,6 +4,8 @@ import { ProjectData } from "../../models/project-data";
 import {ProjectCard} from "../project-card/ProjectCard";
 import space_invaders from "../../assets/images/space_invaders.jpg";
 import snake from "../../assets/images/snake.jpg";
+import minesweeper from "../../assets/images/minesweeper.jpg";
+import no_image from "../../assets/images/no_image.jpg";
 import { ReactComponent as ManPresenting } from "../../assets/animations/projects.svg";
 
 export class Projects extends React.Component {
@@ -19,27 +21,27 @@ export class Projects extends React.Component {
             new ProjectData(
                 'Snake clone',
                 ['Java', 'JavaFx'],
-                '',
+                snake,
                 'https://github.com/HbotondS/Snake'),
             new ProjectData(
                 'Minesweeper clone',
                 ['Qt', 'C++'],
-                snake,
+                minesweeper,
                 'https://github.com/HbotondS/Minesweeper'),
             new ProjectData(
                 '2D Top-down shooter',
                 ['C++', 'OpenGL'],
-                '',
+                no_image,
                 'https://github.com/HbotondS/2D-Top-down-shooter'),
             new ProjectData(
                 'TCP-chat-application',
                 ['Java', 'C++'],
-                '',
+                no_image,
                 'https://github.com/HbotondS/TCP-chat-application'),
             new ProjectData(
                 'MovieDB',
                 ['Kotlin'],
-                '',
+                no_image,
                 'https://github.com/HbotondS/MovieDB-Android'),
         ];
     }
