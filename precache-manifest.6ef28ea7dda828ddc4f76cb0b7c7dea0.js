21510f6c8f2d2528bed31f33cb505b8c4dddbb60
Botond Hegyi <botond.hegyi@msg.group>
2020-11-04 09:14:52 +0200

    Updates

---
+++ precache-manifest.6ef28ea7dda828ddc4f76cb0b7c7dea0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ed23cf87472ced7e3a7aa7b06f73d1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e74f84517fa1202d2629",
+    "url": "/portfolio/static/css/main.5b3c8656.chunk.css"
+  },
+  {
+    "revision": "f40a7f1f2dfd5571f349",
+    "url": "/portfolio/static/js/2.bbe1fb7b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.bbe1fb7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74f84517fa1202d2629",
+    "url": "/portfolio/static/js/main.75abf0ac.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "00ff350321b55c25b463a8271c6b9dcd",
+    "url": "/portfolio/static/media/Botond Hegyi CV.00ff3503.pdf"
+  },
+  {
+    "revision": "96189c899da7ff08b2e6acaa9db02599",
+    "url": "/portfolio/static/media/minesweeper.96189c89.jpg"
+  },
+  {
+    "revision": "ea411ebf6153f5f3201aa5a134949f3e",
+    "url": "/portfolio/static/media/no_image.ea411ebf.jpg"
+  },
+  {
+    "revision": "ac2338075cf98678ea03af7b18a01997",
+    "url": "/portfolio/static/media/projects.ac233807.svg"
+  },
+  {
+    "revision": "028124496a56c4bf6b02897756c35e83",
+    "url": "/portfolio/static/media/reading.02812449.svg"
+  },
+  {
+    "revision": "58a8700979d16af508ed9c9fb6b85f25",
+    "url": "/portfolio/static/media/snake.58a87009.jpg"
+  },
+  {
+    "revision": "5a1170409a4a8d5b85ba87af2abe3743",
+    "url": "/portfolio/static/media/space_invaders.5a117040.jpg"
+  },
+  {
+    "revision": "585d5adbe0b35b95d87d7abf2011afd0",
+    "url": "/portfolio/static/media/thinking_man.585d5adb.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfolio/static/media/work.c1a95f73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.938450e7eeab2e8a7f9bb93cf361f5c9.js"
+  "/portfolio/precache-manifest.6ef28ea7dda828ddc4f76cb0b7c7dea0.js"
 );
 
 self.addEventListener('message', (event) => {
