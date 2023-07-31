30f332b7230cc04d9b4bec9b0269c36dd9fb3ee2
HbotondS <hbotond97@gmail.com>
2023-07-31 17:45:01 +0300

    rewrote experience section to functional components and applied tailwind

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,84 @@
+import './Experience.scss';
+import * as React from 'react';
+import { ReactComponent as ReadingSvg } from '../../assets/animations/reading.svg';
+import { ReactComponent as WorkSvg } from '../../assets/animations/work.svg';
+import { ExperienceCard } from '../experience-card/ExperienceCard';
+import { ExperienceData } from '../../models/experience-data';
+
+export function Experience(props) {
+  const experiences = [
+    new ExperienceData(
+      'Computer Science',
+      {
+        url: 'https://ms.sapientia.ro/en',
+        displayName: 'Sapientia Hungarian Univeristy of Transilvania',
+      },
+      '2016-2018',
+      'Targu Mures, Romania',
+      'education',
+      ''
+    ),
+    new ExperienceData(
+      'Information Science, Erasmus+',
+      {
+        url: 'https://inf.unideb.hu/en',
+        displayName: 'University of Debrecen, Faculty of Informatics',
+      },
+      '2018 feb - 2018 jun',
+      'Debrecen, Hungary',
+      'education',
+      ''
+    ),
+    new ExperienceData(
+      'Software developer intern',
+      {
+        url: 'https://www.msg-systems.ro/',
+        displayName: 'MSG Systems',
+      },
+      '2018 jul - 2018 aug',
+      'Targu Mures, Romania',
+      'work',
+      ''
+    ),
+    new ExperienceData(
+      'Information Science, Bsc degree',
+      {
+        url: 'https://ms.sapientia.ro/en',
+        displayName: 'Sapientia Hungarian Univeristy of Transilvania',
+      },
+      '2018-2020',
+      'Targu Mures, Romania',
+      'education',
+      ''
+    ),
+    new ExperienceData(
+      'Full Stack developer',
+      {
+        url: 'https://www.msg-systems.ro/',
+        displayName: 'MSG Systems',
+      },
+      '2018 nov - Present',
+      'Targu Mures, Romania',
+      'work',
+      'current'
+    ),
+  ];
+
+  return (
+    <div id='experience' className='h-fit flex flex-col bg-stone-900' ref={props.reference}>
+      <ReadingSvg className="reading-svg hidden" />
+      <WorkSvg className="work-svg hidden" />
+      <h1 className='mt-14 ml-16 text-white text-4xl font-semibold'>Experience</h1>
+      <ol className='w-full flex flex-col-reverse items-center'>
+        {experiences.map((experience) => (
+          <li className="border-l-2 border-[#772CE8]">
+            <ExperienceCard experience={experience} />
+          </li>
+        ))}
+      </ol>
+      <div className="w-full">
+        <div className="relative flex flex-col gap-3 mt-5 justify-self-center"></div>
+      </div>
+    </div>
+  );
+}
--- src/theme/themes.js
+++ src/theme/themes.js
@@ -5,13 +5,8 @@ export const lightTheme = {
     bg_light: '#f8f8f8',
     header: '#333333',
     primary: '#772ce8',
-    card_bg: '#EFEEFF',
     title: '#333333',
     text: '#333333',
-    experience_title: '#4E44DC',
-    experience_link: '#333333',
-    experience_date: '#4E44DC',
-    experience_location: 'rgba(78, 68, 220, 0.75)'
 }
 
 export const darkTheme = {
@@ -19,13 +14,8 @@ export const darkTheme = {
     bg_light: '#161515',
     header: '#333333',
     primary: '#772CE8',
-    card_bg: '#333333',
     title: 'white',
     text: 'white',
-    experience_title: 'white',
-    experience_link: '#4E44DC',
-    experience_date: 'white',
-    experience_location: 'rgba(255, 255, 255, 0.75)'
 }
 
 export const GlobalStyles = createGlobalStyle`
@@ -35,46 +25,6 @@ export const GlobalStyles = createGlobalStyle`
     
     #container {
         background: ${(props => props.theme.header)};
-    }
-    
-    .ExperiencePage {
-        background: ${(props => props.theme.bg_light)};
-        
-        .experience-container > .title-container > .title {
-            color: ${(props => props.theme.title)};
-        }
-    }
-    
-    .ExperienceCard {
-        background: ${(props => props.theme.card_bg)};
-        
-        .line {
-            border-left-color: ${(props => props.theme.primary)};
-        }
-        
-        .experience-title {
-            color: ${(props => props.theme.experience_title)};
-        }
-        
-        .experience-link {
-            color: ${(props => props.theme.experience_link)};
-        }
-        
-        .experience-date {
-            color: ${(props => props.theme.experience_date)};
-        }
-        
-        .experience-location {
-            color: ${(props => props.theme.experience_location)};
-        }
-        
-        .circle {
-            border-color: ${(props => props.theme.primary)};
-            
-            &.current {
-                background: ${(props => props.theme.primary)};
-            }
-        }
     }
     
     .ClickMe {
