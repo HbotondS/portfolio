02697a7b31aa2f4ea3d9136d9cbac3e4fabf3a39
HbotondS <hbotond97@gmail.com>
2023-08-17 16:59:11 +0300

    removed unused import

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { Header } from "./components/Header";
